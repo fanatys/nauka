1f898a634661423446b87583db1d7ea81badfbbd
fanatys <klaudia.kubicz@gmail.com>
2022-06-21 18:41:08 +0200

    added dwo files

---
+++ w3_python_variables.py
@@ -0,0 +1,13 @@
+
+x = 5
+y = "John"
+
+print(type(x))
+print(type(y))
+
+o, b, a = "orange", "banana", "apple"
+print(o, b, a)
+
+fruits = ["cherry", "watermelon", "melon"]
+c, w, m = fruits
+print(c, w, m)
